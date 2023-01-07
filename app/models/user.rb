62116a258828a53e28be3772d8e34ce56b24964c
Roman <roman.cabalum@gmail.com>
2023-01-07 10:33:22 +0800

    edit sidebar, add logout button

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,20 @@ class User < ApplicationRecord
   
   def self.from_omniauth(access_token)
     data = access_token.info
+    token = access_token.credentials['token']
     users = User.where(email: data['email']).first
+    name = data['name']
 
     # Uncomment the section below if you want users to be created if they don't exist
      unless users
          users = User.create(
             email: data['email'],
             password: Devise.friendly_token[0,20],
+            name: name,
+            auth_key_google: token
          )
     end
+    
     users
   end
 end
