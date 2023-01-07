907437994de129b16a9156d8b1379ac073b3d2d7
Roman <roman.cabalum@gmail.com>
2023-01-07 10:00:53 +0800

    add sidebar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+   has_many :jobs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
-
+  
   def self.from_omniauth(access_token)
     data = access_token.info
     users = User.where(email: data['email']).first
