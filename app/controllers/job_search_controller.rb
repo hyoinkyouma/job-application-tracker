c17681b123a90a242cbaff47b81a6972db1631a7
mspuy3 <mspuy3@gmail.com>
2023-01-14 02:24:57 +0800

    edited stylings

--- app/controllers/job_search_controller.rb
+++ app/controllers/job_search_controller.rb
@@ -55,7 +55,7 @@ class JobSearchController < ApplicationController
         @job.job_title = params[:job_title]
         @job.job_description = params[:job_description]
         @job.salary = 0.00  
-        @job.status = 1
+        @job.status = 0
         @job.accepted = false
         @job.user_id = current_user.id
 
@@ -72,6 +72,30 @@ class JobSearchController < ApplicationController
         location = params[:anything][:location]
         isRemote = params[:anything][:isRemote] == 0 ? false : true
         @result = searchLinkedIn(keyword, location, isRemote)
+
+        job_tally = @jobs.size
+        job_inprogress = 0
+        job_accepted = 0
+        job_rejected = 0
+        
+        @jobs.each do |job|
+            if job.status.between?(0,3)
+                job_inprogress += 1
+            elsif job.status == 4
+                if job.accepted
+                    job_accepted +=1
+                else
+                    job_rejected +=1
+                end
+            end
+        end
+
+        @trackers = {
+            tally: job_tally,
+            inprogress: job_inprogress,
+            accepted: job_accepted,
+            rejected: job_rejected
+        }
     end
 
     private
